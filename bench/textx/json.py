ffbebc21cfcf9b4fe8aebea4863671e195cdbe7c
Bryan Hu <bryan.hu.2020@gmail.com>
2021-04-19 10:58:39 -0700

    faster via packrat parsing
    May usee more memory

--- bench/textx/json.py
+++ bench/textx/json.py
@@ -3,47 +3,37 @@ from os.path import join, dirname
 import collections
 from bench import helpers
 
-this_folder = dirname(__file__)
-
-
-class Member:
-    def __init__(self, key, value, parent=None):
-        self.pair = (key, value)
-
-    def __repr__(self):
-        return str(self.pair)
-
-    def __iter__(self):
-        return iter(self.pair)
-
-
-class Object(collections.UserDict):
-    def __init__(self, members, parent=None):
-        self.data = {key: value for key, value in members}
-
-
-# class Array:
-#     def __init__(self, values, parent=None):
-#         self.value = list(values)
-#
-#     def __repr__(self):
-#         return str(self.value)
-class Array(collections.UserList):
-    def __init__(self, values, parent=None):
-        self.data = list(values)
-
 
 def compile():
+    class Member:
+        def __init__(self, key, value, parent=None):
+            self.pair = (key, value)
+
+        def __repr__(self):
+            return str(self.pair)
+
+        def __iter__(self):
+            return iter(self.pair)
+
+    class Object(collections.UserDict):
+        def __init__(self, members, parent=None):
+            self.data = {key: value for key, value in members}
+
+    class Array(collections.UserList):
+        def __init__(self, values, parent=None):
+            self.data = list(values)
+
     json_mm = metamodel_from_file(
-        join(this_folder, "json.tx"),
+        join(dirname(__file__), "json.tx"),
         debug=False,
         classes=[Member, Object, Array],
+        memoization=True,
     )
 
     json_mm.register_obj_processors(
         {
             "NULL": lambda _: None,
-            "STRING": lambda x: helpers.json_unescape(x),
+            "STRING": helpers.json_unescape,
             "FLOAT": float,
             "BOOL": lambda x: x == "true",
         }
