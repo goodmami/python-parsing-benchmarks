f11a06b0969cb39cedd10e52f5261b9ea3d0f36c
Erez Shinan <erezshin+git@gmail.com>
2022-01-31 11:00:11 +0200

    Added lark_cython to the benchmarks

---
+++ bench/lark_cython/json.py
@@ -0,0 +1,76 @@
+from lark import Lark, Transformer, v_args
+from lark_cython import lark_cython
+
+from bench.helpers import json_unescape
+
+
+def compile():
+    # NOTE: the ; after value is to detect the end of the input
+    json_grammar = r"""
+        ?start: _WS? value _WS? ";"
+
+        ?value: object
+              | array
+              | string
+              | NUMBER             -> number
+              | "true"             -> true
+              | "false"            -> false
+              | "null"             -> null
+
+        array  : _BRACK1 [value (_COMMA value)*] _BRACK2
+        object : _CURLY1 [pair (_COMMA pair)*] _CURLY2
+        pair   : string _COLON value
+
+        _COLON: /\s*:\s*/
+        _COMMA: /\s*,\s*/
+        _CURLY1: /\s*{\s*/
+        _CURLY2: /\s*}\s*/
+        _BRACK1: /\s*\[\s*/
+        _BRACK2: /\s*\]\s*/
+
+        string : STRING
+        STRING: "\"" INNER* "\""
+        INNER: /[ !#-\[\]-\U0010ffff]*/
+             | /\\(?:["\/\\bfnrt]|u[0-9A-Fa-f]{4})/
+
+        NUMBER : INTEGER FRACTION? EXPONENT?
+        INTEGER: ["-"] ("0" | "1".."9" INT?)
+        FRACTION: "." INT
+        EXPONENT: ("e"|"E") ["+"|"-"] INT
+
+        _WS: /\s+/
+
+        %import common.INT
+    """
+
+    class TreeToJson(Transformer):
+        @v_args(inline=True)
+        def string(self, s):
+            return json_unescape(s.value)
+
+        array = list
+        pair = tuple
+        object = dict
+
+        @v_args(inline=True)
+        def number(self, n):
+            return float(n.value)
+
+        null = lambda self, _: None
+        true = lambda self, _: True
+        false = lambda self, _: False
+
+    json_parser = Lark(json_grammar,
+                       parser='lalr',
+                       lexer='basic',
+                       propagate_positions=False,
+                       maybe_placeholders=False,
+                       transformer=TreeToJson(),
+                       _plugins=lark_cython.plugins,
+                       )
+
+    # trailing ; is currently necessary to detect end of input
+    return lambda s: json_parser.parse(s + ';')
+
+
+parse = compile()
